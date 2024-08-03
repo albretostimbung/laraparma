3638ca1a479f8b57be4f4f12dc5886718a60b82c
Albreto Suryanata Timbung <albreto.suryanata@ifg-life.id>
2024-08-03 13:58:51 +0700

    feat: add role seeder

--- database/migrations/2024_08_03_062252_create_product_transactions_table.php
+++ database/migrations/2024_08_03_062252_create_product_transactions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('total_amount');
             $table->boolean('is_paid');
             $table->string('address');
