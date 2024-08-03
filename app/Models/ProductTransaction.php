67be3866002a94e72964f2f0e93434470b367374
Albreto Suryanata Timbung <albreto.suryanata@ifg-life.id>
2024-08-03 13:29:43 +0700

    feat: create table migrations

---
+++ app/Models/ProductTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductTransaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+}
